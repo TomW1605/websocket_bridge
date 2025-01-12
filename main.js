ace01216e0a9bf1add54f2b8790958159030096b
Thomas White <TomW1605@users.noreply.github.com>
2025-01-12 11:20:36 +0800

    added more detailed log messages

--- main.js
+++ main.js
@@ -10,19 +10,21 @@ wss.on('connection', function connection(ws) {
 	ws.on('error', console.error);
 
 	ws.on('message', function message(data, isBinary) {
+		let remoteAddress = ws._socket.remoteAddress.replace("::ffff:", "");
 		if (isBinary) {
-			console.log('received binary data from %s, discarding', ws._socket.remoteAddress);
+			console.log('received binary data from %s:%i, discarding', remoteAddress, ws._socket.remotePort);
 			return;
 		}
-		console.log('received "%s" from %s', data, ws._socket.remoteAddress);
+		console.log('received "%s" from %s:%i', data, remoteAddress, ws._socket.remotePort);
 		wss.clients.forEach(function(client) {
 			let data_str = `${data}`;
 			let echo_request = data_str.toLowerCase().startsWith("echo ");
 			if (echo_request) {
-				data_str = data_str.substring(4);
+				data_str = data_str.substring(5);
 			}
 			if ((client !== ws && client.readyState === WebSocket.OPEN) || echo_request) {
 				client.send(data_str);
+				console.log('sent "%s" to %s:%i', data_str, client._socket.remoteAddress.replace("::ffff:", ""), client._socket.remotePort);
 			}
 		});
 	});
